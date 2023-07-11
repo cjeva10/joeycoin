5545dcfc5659f0e9ac212c5da5cf4a157b7cd9ba
ceva77 <cevanko@binnacle.org>
2023-07-11 11:02:07 -0400

    implement basic PoW algorithm

---
+++ miner/miner.go
@@ -0,0 +1,67 @@
+// functions for mining blocks
+
+package miner
+
+import (
+	"crypto/sha256"
+	"encoding/binary"
+	"math/big"
+
+	"github.com/cjeva10/joeycoin/types"
+)
+
+// an interface for generating a proof of work
+type Proof interface {
+	Prove() [32]byte
+	Valid() bool
+}
+
+type BlockProof struct {
+	Block *types.Block
+	Miner *types.Account
+	Work  int64 // nonce
+}
+
+// generate a proof of work by hashing the block hash with the nonce
+func (proof *BlockProof) Prove() [32]byte {
+	workBytes := make([]byte, 8)
+	binary.BigEndian.PutUint64(workBytes, uint64(proof.Work))
+
+	blockBytes := append(proof.Block.Hash[:], proof.Miner.Bytes()...)
+	blockBytes = append(blockBytes, workBytes...)
+
+	return sha256.Sum256(blockBytes)
+}
+
+// check if a proof is valid
+func (proof *BlockProof) Valid() bool {
+	proofInt := new(big.Int)
+	prove := proof.Prove()
+	proofInt.SetBytes(prove[:])
+
+	DIFFICULTY := []byte{0, 0, 128, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255}
+	diffInt := new(big.Int)
+	diffInt.SetBytes(DIFFICULTY)
+
+	if -1 == proofInt.Cmp(diffInt) {
+		return true
+	}
+
+	return false
+}
+
+// create a valid proof of work for a given block and account
+func BuildProof(block *types.Block, account *types.Account) BlockProof {
+    proof := BlockProof{
+        Block: block,
+        Miner: account,
+        Work: 0,
+    }
+
+    // simply keep incrementing the work until the proof is valid
+    for !proof.Valid() {
+        proof.Work++
+    }
+
+    return proof
+}
---
+++ miner/miner_test.go
@@ -0,0 +1,57 @@
+package miner
+
+import (
+	"crypto/ecdsa"
+	"crypto/elliptic"
+	"crypto/rand"
+	"crypto/sha256"
+	"math/big"
+	"testing"
+	"time"
+
+	"github.com/cjeva10/joeycoin/types"
+)
+
+func TestProof(t *testing.T) {
+	now := big.NewInt(time.Now().Unix())
+	digest := append(big.NewInt(0).Bytes(), now.Bytes()...)
+	digest = append(digest, nil...)
+
+	reader := rand.Reader
+	priv1, err := ecdsa.GenerateKey(elliptic.P256(), reader)
+	if err != nil {
+		t.Fatalf("Failed to generate a private key")
+	}
+
+	pub1 := priv1.PublicKey
+
+	account1 := types.Account{
+		Address: &pub1,
+		Balance: big.NewInt(1000),
+		Nonce:   big.NewInt(0),
+	}
+
+	genesis := types.Block{
+		Body:      nil,
+		PrevHash:  [32]byte{},
+		Hash:      sha256.Sum256(digest),
+		Number:    big.NewInt(0),
+		Timestamp: now,
+	}
+
+	// generate the proof
+	genesisProof := &BlockProof{
+		Block: &genesis,
+		Miner: &account1,
+		Work:  0,
+	}
+
+	i := 0
+	for !genesisProof.Valid() {
+		// increase the nonce
+		genesisProof.Work++
+		i++
+	}
+
+	t.Logf("Generated valid proof after %d iterations", i)
+}
